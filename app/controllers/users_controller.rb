d7087df8437df26927684e1fe615a89ac72ad0fb
kadia <kdabraeux@gmail.com>
2021-10-20 20:40:20 +0000

    signup and login fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,20 @@ class UsersController < ApplicationController
   end
 
   def create
+  
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
+      # puts "error, no valid entry#{user.errors.full_messages}"
       redirect_to '/signup'
+
     end
   end
 
     private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
